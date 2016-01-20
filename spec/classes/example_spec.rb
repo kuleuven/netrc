f6287019b4a9f8a0960a9e86fd3610ad72d9b2ba
David Hayes <david.hayes@s3group.com>
2016-01-20 12:40:53 +0000

    Basic spec testing

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -8,14 +8,42 @@ describe 'netrc' do
           facts
         end
         
-        let(:params) do
-          {
-            :credentials => [{'machine' => 'foo.com', 'login' => 'foobar', 'password' => 'hunter5'}]
-          }
-        end
         
-        context "netrc class with credentials array" do
+        context "netrc class with credentials and user params" do
+          let(:params) do
+            {
+              :credentials => [{'machine' => 'foo.com', 'login' => 'foobar', 'password' => 'hunter5'}],
+              :user        => 'myuser',
+            }
+          end
+
           it { is_expected.to compile.with_all_deps }
+
+          it do
+            should contain_file('/home/myuser/.netrc').with({
+              'mode' => '0600',
+              'owner' => 'myuser',
+            })
+          end
+        end
+
+        context "netrc class with non-default user directory" do
+          let(:params) do
+            {
+              :credentials => [{'machine' => 'foo.com', 'login' => 'foobar', 'password' => 'hunter5'}],
+              :user        => 'otheruser',
+              :user_dir    => '/users'
+            }
+          end
+
+          it { is_expected.to compile.with_all_deps }
+
+          it do
+            should contain_file('/users/otheruser/.netrc').with({
+              'mode' => '0600',
+              'owner' => 'otheruser',
+            })
+          end
         end
       end
     end
@@ -36,7 +64,7 @@ describe 'netrc' do
         }
       end
       
-      it { expect { should compile }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+      it { expect { should compile }.to raise_error(/Nexenta not supported/) }
     end
   end
 end
