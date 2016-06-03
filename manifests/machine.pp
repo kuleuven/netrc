584941bc8cbd12d80169b9a9a54226176939afa8
David Hayes <evidex@users.noreply.github.com>
2016-06-03 23:37:02 +0100

    Rename netrc_path to path for netrc::machine (#7)
    https://github.com/code-management/netrc/issues/6

--- manifests/machine.pp
+++ manifests/machine.pp
@@ -7,10 +7,13 @@
 # ----------
 #
 # * `machine`
-#   Remote machine this .netrc is targetted at. Default: $name
+#   Remote machine this .netrc is targeted at. Default: $name
+#
+# * `path`
+#   Required. Path to the .netrc file to write. Should match that specified in the related `netrc` resource.
 #
 # * `netrc_path`
-#   Path to .netrc file.
+#   Equivalent to `path`. Deprecated in v1.0.0
 #
 # * `login`
 #   Login user on remote machine
@@ -22,28 +25,40 @@
 # -------
 #
 # netrc::machine { 'foo.bar.com':
-#   netrc_path => '/home/myuser/.netrc',
-#   login      => 'remote_user',
-#   password   => 'hunter5',
+#   path     => '/home/myuser/.netrc',
+#   login    => 'remote_user',
+#   password => 'hunter5',
 # }
 #
 define netrc::machine (
-  $netrc_path,
   $login,
   $password,
+  $path         = undef,
+  $netrc_path   = undef,
   $machine      = $name,
 ) {
 
   # validate parameters here
   validate_string($machine)
   validate_string($netrc_path)
+  validate_string($path)
   validate_string($login)
   validate_string($password)
 
+  if $path == undef {
+    if $netrc_path != undef {
+      warning('[DEPRECATED] `netrc_path` parameter is deprecated in v1.0.0. Use `path` instead')
+      $target_path = $netrc_path
+    } else {
+      fail('`path` parameter is required, and should match that of the netrc resource')
+    }
+  } else {
+    $target_path = $path
+  }
 
   # Write the .netrc file
   concat::fragment{ "netrc_fragment_${machine}":
-    target  => $netrc_path,
+    target  => $target_path,
     content => template('netrc/netrc.erb'),
   }
 }
